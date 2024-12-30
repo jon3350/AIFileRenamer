0520a9e76a984f1fd8858fab0ce11088abe7b3bf
peterAKG <peter.gretchikha@yahoo.com>
2024-12-29 22:51:13 -0500

    Dynamically loaded css style sheet with javascript

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import fs from 'fs';
 
 http.createServer(function (req, res) {
     var url = req.url;
+    console.log(`Request URL: ${req.url}`);
     if (url === "/") {
         fs.readFile("head.html", function (err, pgres) {
             if (err) {
@@ -13,7 +14,7 @@ http.createServer(function (req, res) {
                 res.write("HEAD.HTML NOT FOUND");
                 res.end();
             } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.writeHead(200, {'Content-Type': 'text/html' });
                 res.write(pgres);
                 res.end();
             }
@@ -30,6 +31,10 @@ http.createServer(function (req, res) {
                 res.end();
             }
         });
+    } else if (req.url == '/style.css') {
+        res.setHeader('Content-type', 'text/css');
+        res.write(fs.readFileSync('styles.css'));
+        res.end();
     }
 
 }).listen(3000, function () {
