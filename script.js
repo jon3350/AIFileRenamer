d42ab6b69ad3592d62a2d2427060f5611d19ce91
jon3350 <john2015.wu@gmail.com>
2024-12-29 23:43:24 -0500

    make the script do something interactive

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// const h1Element = document.querySelector('#test123');
-// h1Element.addEventListener('onclick', () => alert('clicked'))
-// document.addEventListener('onclick', e => alert('clicked'))
-console.log('script')
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    // Select the component by its ID
+    const button = document.getElementById('test123');
+    
+    // Add a click event listener to the button
+    button.addEventListener('click', () => {
+        // Alert a message when the button is clicked
+        alert('Button was clicked!');
+    });
+});
